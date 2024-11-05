a1c1b288a349985c5ffcfb23a2bcd4394a25dd45
matheoFauchez <matheo.fauchez@alumni.univ-avignon.fr>
2024-11-05 10:38:25 +0100

    Ex3, tentative 5 correction coverage => ajout test echangerLesDeuxMemesNoeuds

--- src/test/java/liste/ListeSimpleTest.java
+++ src/test/java/liste/ListeSimpleTest.java
@@ -255,4 +255,11 @@ public class ListeSimpleTest {
         System.out.println(listeATester);
         assertEquals(listeATester.toString(), "ListeSimple(Noeud(4), Noeud(2), Noeud(3), Noeud(1), Noeud(5))");
     }
+
+    @Test
+    public void echangerLesDeuxMemesNoeuds()
+    {
+        Noeud r1 = listeATester.tete;
+        listeATester.echanger(r1, r1);
+    }
 }
